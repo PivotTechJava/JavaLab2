20fdb9dcb9ce2d5b708ddb5f05b02f80bf6ba9ae
Shaun Carter <why06mail@gmail.com>
2021-07-27 17:55:35 -0500

    containsDuplicates added

---
+++ src/com/pivottech/ContainsDuplicates.java
@@ -0,0 +1,19 @@
+package com.pivottech;
+
+public class ContainsDuplicates {
+
+
+    public static void main(String[] args) {
+
+        // change this array as coding to see if it works for a case where there are no duplicates
+        int[] testArray = {1,2,3,3,4,5};
+
+        // prints if contains duplicates
+        System.out.println(containsDuplicates(testArray) ? "contains duplicates" : "contains NO duplicates");
+    }
+
+    private static boolean containsDuplicates(int[] arr) {
+        // your code here
+        return false; // replace this with right answer
+    }
+}
