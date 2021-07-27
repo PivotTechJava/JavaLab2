f3222cb53525b34c7e4367ab2fe3548057ba09ea
Shaun Carter <why06mail@gmail.com>
2021-07-27 17:49:42 -0500

    init commit

---
+++ src/com/pivottech/RollingDice.java
@@ -0,0 +1,14 @@
+package com.pivottech;
+
+public class RollingDice {
+
+    public static void main(String[] args) {
+	    // rolls 6 times
+        System.out.println(roll(6));
+    }
+
+    private static int[] roll(int rollCount) {
+        // your code here
+        return null; // replace this with your integer array
+    }
+}
