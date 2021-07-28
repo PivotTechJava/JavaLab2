3ac5652310d6e41527b8715a0d2ab0074bf721a7
Austin Cumberlander <austincumberlander@gmail.com>
2021-07-27 20:22:19 -0500

    Added new method that uses ArrayList instead of Set.

--- src/com/pivottech/ContainsDuplicates.java
+++ src/com/pivottech/ContainsDuplicates.java
@@ -1,5 +1,6 @@
 package com.pivottech;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,11 +14,22 @@ public class ContainsDuplicates {
         int[] testArray = {1,2,3,3,4,5};
 
         // prints if contains duplicates
-        System.out.println(containsDuplicates(testArray) ? "contains duplicates" : "contains NO duplicates");
+        System.out.println(containsDuplicatesArr(testArray) ? "contains duplicates" : "contains NO duplicates");
     }
 
-    private static boolean containsDuplicates(int[] arr) {
+    // uses Set
+    private static boolean containsDuplicatesSet(int[] arr) {
         Set<Integer> lump = new HashSet<Integer>();
+        for (int i : arr) {
+            if (lump.contains(i)) return true;
+             lump.add(i);
+        }
+        return false;
+    }
+
+    // uses ArrayList
+    private static boolean containsDuplicatesArr(int[] arr) {
+        ArrayList<Integer> lump = new ArrayList<Integer>();
         for (int i : arr) {
             if (lump.contains(i)) return true;
             lump.add(i);
