1a1b663cd88b0b1f4551e4165f3aecb256ab7791
mcity42 <city.malik@yahoo.com>
2021-07-27 20:18:03 -0500

    Lab 2

--- src/com/pivottech/RollingDice.java
+++ src/com/pivottech/RollingDice.java
@@ -1,14 +1,32 @@
 package com.pivottech;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class RollingDice {
 
     public static void main(String[] args) {
-	    // rolls 6 times
-        System.out.println(roll(6));
+        // write your code here
+        //System Calls method "roll" with the number rolls given
+        roll(5);
     }
+    public static int roll( int n)
+    {
+        //Array list is initialized and random is created
+        ArrayList <Integer> rollResult = new ArrayList<Integer>(n);
+        Random rand = new Random();
 
-    private static int[] roll(int rollCount) {
-        // your code here
-        return null; // replace this with your integer array
+        //Loop that generate random number with 10 being max and will add value to Array List
+        for(int i = 0; i < n;i++)
+        {
+
+            int x = rand.nextInt(6)+1;
+            rollResult.add(x);
+
+
+        }
+
+        //System prints array list
+        System.out.println(rollResult);
+        return n;
     }
 }
