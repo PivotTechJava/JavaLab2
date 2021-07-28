b17d0e6ccba767b6661ad8bccdbd4c3d80e2e400
jam hawk <jhawkins0425@gmail.com>
2021-07-27 20:27:48 -0500

    Lab2

--- src/com/pivottech/ContainsDuplicates.java
+++ src/com/pivottech/ContainsDuplicates.java
@@ -1,19 +1,33 @@
 package com.pivottech;
 
+import java.io.OptionalDataException;
+
 public class ContainsDuplicates {
 
 
     public static void main(String[] args) {
 
         // change this array as coding to see if it works for a case where there are no duplicates
-        int[] testArray = {1,2,3,3,4,5};
+        int[] testArray = {1, 2, 3, 4, 4, 5};
 
         // prints if contains duplicates
         System.out.println(containsDuplicates(testArray) ? "contains duplicates" : "contains NO duplicates");
     }
 
-    private static boolean containsDuplicates(int[] arr) {
+    private static boolean containsDuplicates(int[] testArray) {
         // your code here
-        return false; // replace this with right answer
+        Boolean x = false;
+
+        for (int i = 0; i < testArray.length; i++) {
+            for (int j = i + 1; j < testArray.length; j++) {
+                if (testArray[i] == testArray[j]) {
+
+                    //  System.out.println("Duplicates");
+                    x = true;
+                }
+                //return true;
+            }
+        }
+        return x; // replace this with right answer
     }
 }
--- src/com/pivottech/RollingDice.java
+++ src/com/pivottech/RollingDice.java
@@ -1,5 +1,8 @@
 package com.pivottech;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class RollingDice {
 
     public static void main(String[] args) {
@@ -7,8 +10,23 @@ public class RollingDice {
         System.out.println(roll(6));
     }
 
-    private static int[] roll(int rollCount) {
+    private static int roll(int rollCount ) {
         // your code here
-        return null; // replace this with your integer array
+        //Array list is initialized and random is created
+        ArrayList<Integer> rollResult = new ArrayList<Integer>(rollCount);
+        Random rand = new Random();
+        //Loop that generate random number with 10 being max and will add value to Array List
+        for(int i = 0; i < rollCount;i++)
+        {
+
+            int x = rand.nextInt(6)+1;
+            rollResult.add(x);
+
+
+        }
+
+        //System prints array list
+        System.out.println(rollResult);
+        return rollCount;
     }
 }
