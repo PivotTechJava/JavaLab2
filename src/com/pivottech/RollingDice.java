81aa2c1319c0773fc275e95771372207ed7cbf7c
carlamiles <ms.carla.albert@gmail.com>
2021-07-27 20:04:26 -0500

    Created code for Rolling Dice

--- src/com/pivottech/RollingDice.java
+++ src/com/pivottech/RollingDice.java
@@ -7,8 +7,23 @@ public class RollingDice {
         System.out.println(roll(6));
     }
 
-    private static int[] roll(int rollCount) {
+    private static int[] roll(int n) {
         // your code here
-        return null; // replace this with your integer array
+
+        // Write a method called roll that accepts an integer n and rolls a six-sided dice n times. The method should return an array containing the values of the rolls.
+
+        // method accepts integer n
+        // need to loop 1 to n times
+        //declare an array
+        ArrayList <int> rollData = new ArrayList <int> ();
+        //create loop that populates array
+        int rollResult;
+        for (int i = 1; i <= n; i++) {
+            rollResult = random.nextInt(6 - 1) + 1;
+            rollData.add(rollResult);
+        }
+
+
+        return rollData; // replace this with your integer array
     }
 }
