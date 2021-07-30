86358f3e7761f0e0d075a2fb587f64fbe4764da4
jam hawk <jhawkins0425@gmail.com>
2021-07-29 19:37:55 -0500

    Lab2

--- src/com/pivottech/Array0FArrays.java
+++ src/com/pivottech/Array0FArrays.java
@@ -2,21 +2,33 @@ package com.pivottech;
 
 public class Array0FArrays {
     public static void main(String[] args) {
-        int [] a = {8,4,4,2,5};
-        Array0fArrays(a);
-    }
-    public static float Array0fArrays(int [] weeklyMonthTem)
-    {
-        int num = aver.length;
-        float average = 0;
-        float sum = 0;
-        for(int i = 0; i < num; i++ )
-        {
-            sum += aver[i];
-            average = sum / num;
-        }
-        System.out.println("  "+ average);
-        return average;
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}};
+
+        getAverage(weeklyMonthTemperatures);
     }
 
-}
+
+        public static float getAverage(int[][] arr){
+            float sum = 0;
+            float counter = 0;
+            float avg = 0;
+            for (int i = 0; i < arr.length; i++){
+                for (int j = 0; j < arr[i].length; j++){
+                    sum += arr[i][j];
+                    counter++;
+                    avg = sum / counter;
+
+                }
+            }
+            System.out.println("The average is " + avg);
+            return avg;
+
+        }
+
+
+    }
+
