3d41dbf5064738469f3b6173e4a22adfce6ae7c0
Shaun Carter <why06mail@gmail.com>
2021-07-29 19:33:54 -0500

    example

--- src/com/pivottech/ContainsDuplicates.java
+++ src/com/pivottech/ContainsDuplicates.java
@@ -1,19 +1,40 @@
 package com.pivottech;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
 public class ContainsDuplicates {
 
 
     public static void main(String[] args) {
 
         // change this array as coding to see if it works for a case where there are no duplicates
-        int[] testArray = {1,2,3,3,4,5};
+        Integer[] testArray = {1,2,3,3,4,5};
 
         // prints if contains duplicates
         System.out.println(containsDuplicates(testArray) ? "contains duplicates" : "contains NO duplicates");
     }
 
-    private static boolean containsDuplicates(int[] arr) {
-        // your code here
-        return false; // replace this with right answer
+    private static boolean containsDuplicates(Integer[] arr) {
+        HashMap<Integer,Integer> map = new HashMap();
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(arr));
+
+        for (Integer i : arrayList) {
+            Integer count = map.get(i);
+            map.put(i, (count != null ? count + 1 : 0));
+        }
+
+        AtomicBoolean hasDupes = new AtomicBoolean(false);
+        map.forEach((key, val) -> {
+            if(val > 1) hasDupes.set(true);
+        });
+
+        System.out.println(arrayList);
+        System.out.println(hasDupes);
+
+        return hasDupes.getPlain();
     }
 }
